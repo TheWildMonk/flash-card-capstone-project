089793f17d6084782eea9bcccab41ba15e28468c
Shuvo Nasir <shuvonasir028@gmail.com>
2021-08-07 21:54:55 +0600

    add card flashing capability for the translated word

--- main.py
+++ main.py
@@ -13,13 +13,24 @@ WORD_FONT = ("Raleway", 60, "bold")
 # Read csv data
 df_word = pd.read_csv("data/french_words.csv")
 word_dict = df_word.to_dict(orient="records")
+current_card = {}
 
 
 # Generate new word
 def new_card():
+    global current_card, flip_timer
+    root.after_cancel(flip_timer)
     current_card = choice(word_dict)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"].title())
+    canvas.itemconfig(canvas_image, image=old_image)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"].title(), fill="black")
+    flip_timer = root.after(3000, func=translation)
+
+
+def translation():
+    canvas.itemconfig(canvas_image, image=new_image)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"].title(), fill="white")
 
 
 # Window object definition
@@ -28,10 +39,14 @@ root.title("Flash Card")
 root.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 root.resizable(width=False, height=False)
 
+# Constant
+flip_timer = root.after(3000, func=translation)
+
 # Canvas object definition
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-bg_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=bg_image)
+old_image = PhotoImage(file="images/card_front.png")
+new_image = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=old_image)
 card_title = canvas.create_text(400, 150, text="Title", font=LANGUAGE_FONT)
 card_word = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 
@@ -51,6 +66,5 @@ wrong_button.grid(row=1, column=0)
 
 # Initial run of the app
 new_card()
-
 # Window mainloop
 root.mainloop()
